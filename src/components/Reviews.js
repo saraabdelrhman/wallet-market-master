4b88bc0845632b8fd309ec4d8c55856239a671b2
saraabdelrhman <saraabdelrhman162@gmail.com>
2024-10-20 16:53:19 +0300

    toggle button and reviews page responsive

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // <-- Import useNavigate
+import { useNavigate } from "react-router-dom"; 
 import img1 from './images/image 6 (1).png';
 import img2 from './images/image 6 (2).png';
 import img3 from './images/image 6 (3).png';
@@ -15,34 +15,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStar as faStarEmpty } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
-export default function Reviews({content}) {
+export default function Reviews({ content }) {
   const [reviews, setReviews] = useState([
-    { name: "Gerald ",role:'Expert 🎖️', stars: 5, review: "I love this product! easy to user and super cozy!", image: anonymous, date: "Now" },
-    { name: "Diana ",role:'Editor 🏆', stars: 4, review: "Super efficient service and delivery.", image: img2, date: "4 days ago" },
-    { name: "Andre ", role:'User 🏅 ',stars: 4, review: "Amazing delivery speed and service quality.", image: img3, date: "27 Aug 2024" }
+    { name: "Gerald ", role: 'Expert 🎖️', stars: 5, review: "I love this product! easy to use and super cozy!", image: anonymous, date: "Now" },
+    { name: "Diana ", role: 'Editor 🏆', stars: 4, review: "Super efficient service and delivery.", image: img2, date: "4 days ago" },
+    { name: "Andre ", role: 'User 🏅 ', stars: 4, review: "Amazing delivery speed and service quality.", image: img3, date: "27 Aug 2024" }
   ]);
 
   const [newReview, setNewReview] = useState({ name: '', stars: 0, review: '', isAnonymous: false });
   const [showModal, setShowModal] = useState(false);
-  const [loading, setLoading] = useState(false); // <-- Add loading state
+  const [loading, setLoading] = useState(false); 
+  const navigate = useNavigate();
 
-  const navigate = useNavigate(); // <-- Initialize useNavigate hook
-
-  // Function to handle image upload
   const handleImageUpload = (e) => {
-    const file = e.target.files[0];  // Get the first selected file
+    const file = e.target.files[0];
     if (file) {
-      setLoading(true); // <-- Set loading state to true
-      const reader = new FileReader();  // Create a new FileReader to read the file
+      setLoading(true);
+      const reader = new FileReader();
       reader.onloadend = () => {
-        setNewReview((prevReview) => ({ ...prevReview, image: reader.result }));  // Update the newReview state with the uploaded image
-        setLoading(false); // <-- Set loading state to false after upload
+        setNewReview((prevReview) => ({ ...prevReview, image: reader.result }));
+        setLoading(false);
       };
-      reader.readAsDataURL(file);  // Read the file as a data URL
+      reader.readAsDataURL(file);
     }
   };
 
-  // Updated handleSubmit function with navigation
   const handleSubmit = (e) => {
     e.preventDefault();
     if (newReview.name && newReview.stars > 0 && newReview.review) {
@@ -51,7 +48,7 @@ export default function Reviews({content}) {
       setReviews(updatedReviews);
       setNewReview({ name: '', stars: 0, review: '', isAnonymous: false });
       setShowModal(false);
-      navigate('/thanks');  // <-- Redirect to the "thanks" page after submission
+      navigate('/thanks');  
     }
   };
 
@@ -71,7 +68,7 @@ export default function Reviews({content}) {
     <div className="container w-100">
       <header className="header" style={{ backgroundColor: 'transparent' }}>
         <h1 className="text-center mt-5 mb-5 fw-bold">
-        {content["best-rated"]}
+          {content["best-rated"]}
         </h1>
       </header>
 
@@ -87,6 +84,9 @@ export default function Reviews({content}) {
                 <div style={reviewCountStyle}>2,700 User reviews</div>
               </div>
               <div className="review-rating" style={ratingStyle}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" style={{ width: '60px', height: '60px', position: 'relative', cursor: 'pointer' }}>
+                  <polygon points="50,15 61,35 82,35 67,50 73,70 50,58 27,70 33,50 18,35 39,35" style={{ fill: 'white', stroke: '#ADB7BC', strokeWidth: 7 }} />
+                </svg>
                 <div style={ratingNumberStyle}>4.8</div>
               </div>
             </div>
@@ -105,8 +105,8 @@ export default function Reviews({content}) {
 
             <div style={starRatingStyle}>
               {[...Array(5)].map((_, index) => (
-                <svg key={index} xmlns="http://www.w3.org/2000/svg" style={{ width: '33px' }} viewBox="0 0 24 24" fill="#377BF7">
-                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                <svg key={index} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" style={{ width: '50px', height: '50px', position: 'relative', cursor: 'pointer' }}>
+                  <polygon points="50,15 61,35 82,35 67,50 73,70 50,58 27,70 33,50 18,35 39,35" style={{ fill: 'white', stroke: '#ADB7BC', strokeWidth: 7 }} />
                 </svg>
               ))}
             </div>
@@ -147,67 +147,39 @@ export default function Reviews({content}) {
             <div style={starsContainerStyle} className="py-3">
               {renderStarsForForm(newReview.stars, handleStarClick)}
             </div>
-            
+
             {/* Toggle for anonymous post */}
-            <div style={{ 
-              width: '100%', 
-              paddingTop: 18, 
-              paddingBottom: 18, 
-              paddingLeft: 24, 
-              paddingRight: 12, 
-              background: 'rgba(55, 123, 247, 0.20)', 
-              borderRadius: 7, 
-              justifyContent: 'space-between', 
-              alignItems: 'center', 
-              display: 'inline-flex' 
-            }}>
-              <div style={{ 
-                color: '#474545', 
-                fontSize: 16, 
-                fontFamily: 'Roboto', 
-                fontWeight: '500', 
-                letterSpacing: 0.15, 
-                wordWrap: 'break-word' 
-              }}>
+            <div style={{ width: '100%', paddingTop: 18, paddingBottom: 18, paddingLeft: 24, paddingRight: 12, background: 'rgba(55, 123, 247, 0.20)', borderRadius: 7, justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex' }}>
+              <div style={{ color: '#474545', fontSize: 16, fontFamily: 'Roboto', fontWeight: '500', letterSpacing: 0.15, wordWrap: 'break-word' }}>
                 Post anonymously
               </div>
-              
+
               {/* Toggle Switch */}
-              <label className="switch">
-                <input 
-                  type="checkbox" 
-                  checked={newReview.isAnonymous} 
-                  onChange={toggleAnonymous} 
-                />
-                <span className="slider round"></span>
-              </label>
+              <label style={toggleSwitchStyle}>
+  <input type="checkbox" checked={newReview.isAnonymous} onChange={toggleAnonymous} style={{display: 'none'}} />
+  <span style={{...toggleSliderStyle, ...(newReview.isAnonymous ? checkedSliderStyle  : {})}}></span>
+</label>
+
             </div>
 
-            <textarea className="py-3 my-3 pb-5"
-              placeholder="What's your experience?"
-              value={newReview.review}
-              onChange={(e) => setNewReview((prevReview) => ({ ...prevReview, review: e.target.value }))}
-              style={textareaStyle}
-            />
-            <div style={{position:'absolute',bottom:'30%',right:'8%',left:'50',display:'flex',gap:'5px'}}>
+            <textarea className="py-3 my-3 pb-5" placeholder="What's your experience?" value={newReview.review} onChange={(e) => setNewReview((prevReview) => ({ ...prevReview, review: e.target.value }))} style={textareaStyle} />
+
+            <div style={{ position: 'absolute', bottom: '30%', right: '8%', left: '50', display: 'flex', gap: '5px' }}>
               <img src={ai} alt="AI" />
-              <div style={{color: '#377BF7', fontSize: 12, fontFamily: 'Poppins', fontWeight: '500', wordWrap: 'break-word'}}>
-                Try write with AI
-              </div>
+              <div style={{ color: '#377BF7', fontSize: 12, fontFamily: 'Poppins', fontWeight: '500', wordWrap: 'break-word' }}>Try write with AI</div>
             </div>
 
             {/* Image upload input */}
-            <label htmlFor="imageUpload" style={{ cursor: 'pointer', display: 'block', marginBottom: '10px', display:'flex', justifyContent:'space-between' }}>
-              <div className="d-flex justify-content-between " style={{gap:'220px'}}>
-                {/* Check the loading state and update text */}
-                <p className="mt-2">{loading ? <span style={{ color: 'green' }}>Uploading...</span> : 'Attach File'}</p> {/* <-- Updated text */}
+            <label htmlFor="imageUpload" style={{ cursor: 'pointer', display: 'block', marginBottom: '10px', display: 'flex', justifyContent: 'space-between' }}>
+              <div className="d-flex justify-content-between" style={{ gap: '220px' }}>
+                <p className="mt-2">{loading ? <span style={{ color: 'green' }}>Uploading...</span> : 'Attach File'}</p>
                 <div style={uploadButtonStyle}>Upload Image</div>
               </div>
               <input id="imageUpload" type="file" style={{ display: 'none' }} onChange={handleImageUpload} />
             </label>
 
             <div style={modalActionsStyle}>
-           <Link to='/thanks'>  <button onClick={handleSubmit} style={confirmButtonStyle} className="rounded-5">Yes</button></Link> 
+              <Link to='/thanks'>  <button onClick={handleSubmit} style={confirmButtonStyle} className="rounded-5">Yes</button></Link>
               <button onClick={closeModal} style={cancelButtonStyle} className="rounded-5">Cancel</button>
             </div>
           </div>
@@ -218,55 +190,34 @@ export default function Reviews({content}) {
 }
 
 // Reusable Components
-const ReviewItem = ({ name, date, stars, review, image,role }) => (
+const ReviewItem = ({ name, date, stars, review, image, role }) => (
   <div style={reviewItemStyle} className="d-flex">
-    
     <div style={reviewerStyle} className="d-flex">
-     
       <img src={image} alt={name} className="rounded-5 mb-3" />
-     
-  
       <div className="reviewer-details" style={reviewerDetailsStyle}>
-      
-      <div className="d-flex">
-<div className="d-bock">
-  <div className="d-flex">
-  <div className="reviewer-name" style={reviewerNameStyle}>{name}</div>
-  <span className="mt-1 ms-3 " style={{color: '#6F7171',
-fontSize: 20,
-fontFamily: 'Poppins',
-fontWeight: '500'}}>| </span>
-  <div className="mt-1 ms-2" style={{color: '#377BF7',
-fontSize: 20,
-fontFamily: 'Poppins',
-fontWeight: '500',
-wordWrap: 'break-word'}}>{role}</div>
-  </div>
-       
-        
-        <div style={reviewDateStyle}>{date}</div>
+        <div className="d-flex">
+          <div className="d-block">
+            <div className="d-flex">
+              <div className="reviewer-name" style={reviewerNameStyle}>{name}</div>
+              <span className="mt-1 ms-3" style={{ color: '#6F7171', fontSize: 20, fontFamily: 'Poppins', fontWeight: '500' }}>| </span>
+              <div className="mt-1 ms-2" style={{ color: '#377BF7', fontSize: 20, fontFamily: 'Poppins', fontWeight: '500', wordWrap: 'break-word' }}>{role}</div>
+            </div>
+            <div style={reviewDateStyle}>{date}</div>
+          </div>
+          <div className="review-stars mt-5 " style={starsContainerStyle}>{renderStars(stars)}</div>
         </div>
-
-        <div className="review-stars mt-5 " style={starsContainerStyle}>{renderStars(stars)}</div>
-
-        </div>
-       
-     
-     
+      </div>
     </div>
-</div>
-    <hr></hr>
+    <hr />
     <div style={reviewTextStyle}>{review}</div>
-    <div className='d-flex  gap-2 pb-5 ms-2' style={{color:'#377BF7'}} >
-
-<img src={translate} alt={translate} className=""/>
-<span>View Transalation</span>
-</div>
+    <div className='d-flex gap-2 pb-5 ms-2' style={{ color: '#377BF7' }}>
+      <img src={translate} alt={translate} className="" />
+      <span>View Translation</span>
+    </div>
     {name === "Gerald" ? (
-      <div  className=" justify-content-end gap-2 ">
-        <Link to={'/'}> <button type='submit' style={{backgroundColor:'#377BF7',color:'white',borderRadius:'50px', padding:'10px 20px 10px 20px',border:'0px'}} >Edit</button></Link> 
-          <button type='submit' style={{color:'#377BF7',borderRadius:'50px', padding:'10px 20px 10px 20px',border:'1px solid #377BF7'}} >Delete</button>
-      
+      <div className=" justify-content-end gap-2">
+        <Link to={'/'}> <button type='submit' style={{ backgroundColor: '#377BF7', color: 'white', borderRadius: '50px', padding: '10px 20px', border: '0px' }} >Edit</button></Link>
+        <button type='submit' style={{ color: '#377BF7', borderRadius: '50px', padding: '10px 20px', border: '1px solid #377BF7' }}>Delete</button>
       </div>
     ) : (
       <div style={interactionIconsStyle} className=" justify-content-between gap-5">
@@ -284,7 +235,6 @@ wordWrap: 'break-word'}}>{role}</div>
     )}
   </div>
 );
-
 
 // Component to show the star rating overview
 const StarRatingOverview = ({ rating, percentage, color, width }) => (
@@ -305,11 +255,7 @@ const renderStars = (count) => {
   const stars = [];
   for (let i = 0; i < 5; i++) {
     stars.push(
-      <FontAwesomeIcon
-        key={i}
-        icon={i < count ? faStar : faStarEmpty}
-        style={{ color: i < count ? "#FA8232" : "#ADB7BC", fontSize: "20px" }}
-      />
+      <FontAwesomeIcon key={i} icon={i < count ? faStar : faStarEmpty} style={{ color: i < count ? "#FA8232" : "#ADB7BC", fontSize: "20px" }} />
     );
   }
   return stars;
@@ -319,12 +265,7 @@ const renderStarsForForm = (selectedStars, onClick) => {
   const stars = [];
   for (let i = 1; i <= 5; i++) {
     stars.push(
-      <FontAwesomeIcon
-        key={i}
-        icon={i <= selectedStars ? faStar : faStarEmpty}
-        onClick={() => onClick(i)}
-        style={{ color: i <= selectedStars ? "#FA8232" : "#ADB7BC", fontSize: "25px", cursor: "pointer" }}
-      />
+      <FontAwesomeIcon key={i} icon={i <= selectedStars ? faStar : faStarEmpty} onClick={() => onClick(i)} style={{ color: i <= selectedStars ? "#FA8232" : "#ADB7BC", fontSize: "25px", cursor: "pointer" }} />
     );
   }
   return stars;
@@ -404,7 +345,7 @@ const userReviewInfoStyle = {
   display: 'flex',
   alignItems: 'center',
   gap: '14px',
-  borderRadius:'50%'
+  borderRadius: '50%'
 };
 
 const userImageStyle = {
@@ -432,7 +373,7 @@ const writeReviewLinkStyle = {
   fontFamily: 'Poppins',
   fontWeight: '400',
   cursor: 'pointer',
-  textDecoration: 'none', // This removes any underlines or default link styles
+  textDecoration: 'none', 
 };
 
 const starRatingStyle = {
@@ -443,7 +384,7 @@ const starRatingStyle = {
 const interactionIconsStyle = {
   display: 'flex',
   alignItems: 'center',
-  justifyContent:'between',
+  justifyContent: 'space-between',
   marginTop: '15px',
 };
 
@@ -516,12 +457,6 @@ const reviewerStyle = {
   gap: "14px",
 };
 
-const reviewerImageStyle = {
-  width: "68px",
-  height: "68px",
-  borderRadius: "50%",
-};
-
 const reviewerDetailsStyle = {
   display: "flex",
   flexDirection: "column",
@@ -577,20 +512,20 @@ const modalContentStyle = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  backgroundColor:'#f5f7fe'
+  backgroundColor: '#f5f7fe'
 };
 
 const textareaStyle = {
   width: "100%",
-  height:'200px',
-    padding: "10px",
-    color:'#ADABC3',
-  paddingBottom:"150px",
+  height: '200px',
+  padding: "10px",
+  color: '#ADABC3',
+  paddingBottom: "150px",
   fontSize: "16px",
   borderRadius: "10px",
   border: "0px solid #E4E7E9",
   minHeight: "100px",
-  position:'relative'
+  position: 'relative'
 };
 
 const modalActionsStyle = {
@@ -617,77 +552,109 @@ const cancelButtonStyle = {
   borderRadius: "8px",
   cursor: "pointer",
 };
+const toggleSwitchStyle = {
+  position: 'relative',
+  display: 'inline-block',
+  width: '60px',  // Adjusted for wider switch
+  height: '34px', // Adjusted for a taller switch
+  borderRadius: '30px', // Ensures the toggle has rounded sides
+  transition: 'background-color 0.4s',
+  backgroundColor: '#377bf7', // Default background color
+};
+
+const toggleSliderStyle = {
+  position: 'absolute',
+  width:'25px',
+  height:'25px',
+  cursor: 'pointer',
+  top: '4px', // Slightly offset from the top for 3D effect
+  left: '2px', // Stay consistent with the top offset
+  right: '2px',
+  bottom: '2px',
+  backgroundColor: 'white',
+  transition: 'transform 0.4s',
+  borderRadius: '50%', // Makes the slider a circle
+};
+
+// Style when the checkbox is checked
+const checkedSliderStyle = {
+  transform: 'translateX(26px)', // Adjust translation distance to match new width
+  backgroundColor: '#6a6a6b', // Slider color when active
+};
+
 
 const uploadButtonStyle = {
   padding: '10px 20px ',
-  marginleft:'80px',
+  marginleft: '80px',
   background: 'white',
   borderRadius: 5,
   border: '1px #E6E8EC solid',
   justifyContent: 'center',
 };
 
-// Toggle Switch Styles
-const toggleSwitchStyle = `
-  .switch {
-    position: relative;
-    display: inline-block;
-    width: 50px;
-    height: 24px;
+/* Media Queries */
+const mediaQueries = `
+  @media (max-width: 1200px) {
+    .reviews-container {
+      flex-direction: column;
+    }
+
+    .left-section, .right-section {
+      width: 100%;
+    }
+
+    .review-header, .about-card, .review-item {
+      padding: 16px;
+    }
+
+    .reviewer-name {
+      font-size: 18px;
+    }
+
+    .review-stars {
+      justify-content: flex-start;
+    }
   }
 
-  .switch input {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
+  @media (max-width: 768px) {
+    .reviews-container {
+      flex-direction: column;
+    }
 
-  .slider {
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    transition: 0.4s;
-    border-radius: 24px;
-  }
+    .review-header {
+      flex-direction: column;
+      align-items: flex-start;
+    }
 
-  .slider:before {
-    position: absolute;
-    content: "";
-    height: 20px;
-    width: 20px;
-    left: 4px;
-    bottom: 2px;
-    background-color: white;
-    transition: 0.4s;
-    border-radius: 50%;
-  }
+    .review-box {
+      flex-direction: column;
+      align-items: flex-start;
+    }
 
-  input:checked + .slider {
-    background-color: #2196F3;
-  }
+    .review-item {
+      flex-direction: column;
+      align-items: flex-start;
+    }
 
-  input:checked + .slider:before {
-    transform: translateX(26px);
-  }
+    .right-section {
+      margin-top: 20px;
+    }
 
-  .slider.round {
-    border-radius: 34px;
-  }
+    .modal-content {
+      width: 90%;
+    }
 
-  .slider.round:before {
-    border-radius: 50%;
+    .stars-container {
+      justify-content: flex-start;
+    }
   }
 `;
 
-// Append the styles for the toggle switch
+// Append media queries to the document head
 const appendStyles = () => {
   const styleSheet = document.createElement("style");
   styleSheet.type = "text/css";
-  styleSheet.innerText = toggleSwitchStyle;
+  styleSheet.innerText = mediaQueries;
   document.head.appendChild(styleSheet);
 };
 
